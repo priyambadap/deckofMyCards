eb5c1fd3d63cc35c245af4c7d1d853ef570f133a
priyambadap <priyambada.pradhan2@gmail.com>
2018-07-04 11:44:42 +0530

    Update scriptController.js

--- scriptController.js
+++ scriptController.js
@@ -1,5 +1,6 @@
-angular.module('deckOfCardApp', [])
-    .controller('CardController', function() {
+var app = angular.module('deckOfCardApp', []);
+
+    app.controller('CardController', function($scope) {
         var vm = this;
         vm.suits = ["clubs", "spades", "hearts", "diamonds", ];
         vm.ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
@@ -11,7 +12,7 @@ angular.module('deckOfCardApp', [])
             "spades": [],
             "hearts": [],
             "diamonds": []
-        }
+        };
 
         vm.drawCard = function() {
             if (vm.numberOfCard <= 0) {
@@ -71,7 +72,52 @@ angular.module('deckOfCardApp', [])
             vm.drawCard();
         }
 
-        vm.sortCard = function() {
+		vm.deleteCard = function(index) {
+            var card = vm.dack[index];
+            var cardIndex = card.suits + "_" + card.rank;
+
+            switch (card.suits) {
+                case 'clubs':
+                    angular.forEach(vm.cards.clubs, function(val, key) {
+                        if (val.rank == card.rank) {
+                            vm.cards.clubs.splice(key, 1);
+                            return;
+                        }
+                    });
+                    break;
+                case 'spades':
+                    angular.forEach(vm.cards.spades, function(val, key) {
+                        if (val.rank == card.rank) {
+                            vm.cards.spades.splice(key, 1);
+                            return;
+                        }
+                    });
+                    break;
+                case 'hearts':
+                    angular.forEach(vm.cards.hearts, function(val, key) {
+                        if (val.rank == card.rank) {
+                            vm.cards.hearts.splice(key, 1);
+                            return;
+                        }
+                    });
+                    break;
+                case 'diamonds':
+                    angular.forEach(vm.cards.diamonds, function(val, key) {
+                        if (val.rank == card.rank) {
+                            vm.cards.diamonds.splice(key, 1);
+                            return;
+                        }
+                    });
+                    break;
+                default:
+                    break;
+            }
+            vm.dackIndex.splice(vm.dackIndex.indexOf(cardIndex), 1);
+            vm.dack.splice(index, 1);
+            vm.numberOfCard--;
+        }
+		
+		 vm.sortCard = function() {
             vm.cards.clubs.sort(function(a, b) {
                 return vm.ranks.indexOf(a.rank) - vm.ranks.indexOf(b.rank)
             });
@@ -88,6 +134,6 @@ angular.module('deckOfCardApp', [])
             angular.copy(vm.cards, cards);
             angular.copy(cards.clubs.concat(cards.spades).concat(cards.hearts).concat(cards.diamonds), vm.dack);
         }
-
+		
         
     });
